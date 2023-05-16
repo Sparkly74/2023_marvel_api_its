c6df73d45865aff8ce6c2245ddb11f9b4535a036
Sparkly74 <chrisdufetre@yahoo.fr>
2023-05-16 10:57:17 +0200

    Ajout de commentaires

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
-import hashlib
-import time
-import requests
-from flask import Flask, jsonify
+import hashlib # Importer la librairie pour générer un hash
+import time # Importer la librairie pour générer un timestamp
+import requests # importer al librairie requests pour faire des requêtes HTTP
+from flask import Flask, jsonify # Importer la librairie Flask pour créer une API et jsonify pour retourner du JSON
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app = Flask(__name__) # Créer une instance de l'application Flask
+app.config.from_pyfile('config.py') # Chargez la configuration depuis le fichier config.py
 
-PUBLIC_KEY = app.config['MARVEL_PUBLIC_KEY']
-PRIVATE_KEY = app.config['MARVEL_PRIVATE_KEY']
-BASE_URL ='http://gateway.marvel.com/v1/public/'
+PUBLIC_KEY = app.config['MARVEL_PUBLIC_KEY'] # Récupérer la clé publique depuis la configuration
+PRIVATE_KEY = app.config['MARVEL_PRIVATE_KEY'] # Récupérer la clé publique depuis la configuration
+BASE_URL = app.config['BASE_URL'] # Récupérer l'URL de base depuis la configuration config.py
 
-def generate_hash(ts,private_key, public_key):
-    m = hashlib.md5()
-    m.update(f"{ts}{private_key}{public_key}".encode('utf-8'))
-    return m.hexdigest()
+def generate_hash(ts,private_key, public_key): # Définiri une fonction pour générer un hash
+    m = hashlib.md5() # Créer une instance de l'algorithme MD5, SHA 256, SHA 512 ...
+    m.update(f"{ts}{private_key}{public_key}".encode('utf-8')) # Mettre à jour le hash avec le timestamp, la clé privée et la clé publique
+    return m.hexdigest() # Retourner le hash en hexadécimal
 
-@app.route('/')
-def get_characters():
-    ts = str(time.time())
-    hash = generate_hash(ts, PRIVATE_KEY, PUBLIC_KEY)
-    params = {
+@app.route('/characters') # Création d'une route pour récupérer les personnages
+def get_characters(): # Définir une fonction pour récupérer les personnages
+    ts = str(time.time()) # Générer un timestamp
+    hash = generate_hash(ts, PRIVATE_KEY, PUBLIC_KEY) # Générer un hash avec le timestamp, la clé privée et la clé publique
+    params = { # Définir les paramètres de la requête sur la base d'un dictionnaire
         'apikey': PUBLIC_KEY,
         'ts' : ts,
         'hash' : hash,
         'limit' : 100
     }
 
-    response = requests.get(f"{BASE_URL}characters", params=params)
-    return jsonify(response.json())
+    response = requests.get(f"{BASE_URL}characters", params=params) # Faire une requete HTTP GET sur l'URL de base + characters
+    return jsonify(response.json()) # Retourner le JSON de la réponse
 
-app.run(debug=True)
+app.run(debug=True) # Lancer l'application en mode debug
 
 
